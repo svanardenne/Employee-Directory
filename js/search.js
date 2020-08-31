5f21e4809be7c406cdce16b545216dba1aa13894
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-08-31 10:14:03 -0700

    Finalized

--- js/search.js
+++ js/search.js
@@ -6,6 +6,9 @@ const searchbar = document.querySelector('.searchbar');
 const card = document.getElementsByClassName('card');
 const names = document.getElementsByClassName('name');
 
+
+//Compares search value with names in array to create a search function
+//Which filters results
 search.addEventListener('keyup', (e) => {
     let search = searchbar.value.toLowerCase();
     for (i = 0; i < card.length; i++) {
